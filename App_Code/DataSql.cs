5810a94b5353fc67fa7895e45fc492c277e66a66
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-12-09 23:52:48 +0200

    paskaa

---
+++ App_Code/DataSql.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DataSql
+/// </summary>
+public class DataSql
+{
+    public DataSql()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public static DataTable getData()
+    {
+        string sql = "SELECT asioid, lastname, firstname, date FROM lasnaolot";
+        string connStr = ConfigurationManager.ConnectionStrings["DemoxConnectionString"].ConnectionString;
+    }
+}
---
+++ H3543_Tehtava4.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="H3543_Tehtava4.aspx.cs" Inherits="H3543_Tehtava4" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+</asp:Content>
+
